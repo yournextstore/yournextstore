2cbe8bb7fe9ffe31893cc7b470953cfa25a96c5d
Michal Miszczyszyn <michal@mmiszy.pl>
2024-08-05 12:33:26 -0700

    initial commit for open-source repo
    Co-authored-by: zaiste <oh@zaiste.net>

---
+++ src/app/(store)/@modal/(.)cart-overlay/CartAsideDrawer.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState, type ReactNode } from "react";
+import { Drawer, DrawerContent, DrawerTitle } from "@/ui/shadcn/drawer";
+import { useMediaQuery } from "@/ui/shadcn/hooks/useMediaQuery";
+
+export const CartAsideDrawer = ({ children }: { children: ReactNode }) => {
+	const [open, setOpen] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		const onKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "Escape") {
+				router.back();
+			}
+		};
+		document.addEventListener("keydown", onKeyDown);
+		return () => document.removeEventListener("keydown", onKeyDown);
+	}, [router]);
+
+	const timerId = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+	const isDesktop = useMediaQuery("(min-width: 640px)");
+
+	return (
+		<Drawer
+			open={open}
+			shouldScaleBackground={true}
+			direction={isDesktop ? "right" : "bottom"}
+			onOpenChange={(e) => {
+				if (!e) {
+					if (timerId.current) {
+						clearTimeout(timerId.current);
+					}
+					timerId.current = setTimeout(() => {
+						router.back();
+					}, 100);
+				}
+			}}
+		>
+			<DrawerTitle className="sr-only">Shopping cart</DrawerTitle>
+			<DrawerContent
+				className="sm:fixed sm:bottom-0 sm:left-auto sm:right-0 sm:top-0 sm:mt-0 sm:flex sm:h-full sm:w-1/2 sm:flex-col sm:overflow-hidden sm:rounded-none sm:bg-white sm:shadow-xl lg:w-1/3"
+				aria-describedby="cart-overlay-description"
+				onPointerDownOutside={() => {
+					setOpen(false);
+				}}
+				onEscapeKeyDown={() => {
+					setOpen(false);
+				}}
+			>
+				{children}
+			</DrawerContent>
+		</Drawer>
+	);
+};
