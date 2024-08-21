f8c903c74f8c9cb463f3bb4710cc0fded2e1f83a
zaiste <oh@zaiste.net>
2024-08-21 12:45:55 +0200

    feat: initial customization

--- src/ui/nav/NavMenu.tsx
+++ src/ui/nav/NavMenu.tsx
@@ -4,115 +4,33 @@ import * as React from "react";
 
 // TODO https://github.com/radix-ui/primitives/issues/2769
 
-import { useState, type ComponentPropsWithRef, type KeyboardEvent, type PointerEvent } from "react";
-import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { type Config } from "commerce-kit/yns";
 import {
 	NavigationMenu,
-	NavigationMenuContent,
 	NavigationMenuItem,
 	NavigationMenuLink,
 	NavigationMenuList,
-	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
 } from "@/ui/shadcn/navigation-menu";
 import { YnsLink } from "@/ui/YnsLink";
 
-export function NavMenu() {
+type NavLink = Config["nav"]["links"][number];
+
+export function NavMenu({ links }: { links: NavLink[] }) {
 	const [value, setValue] = useState<string | undefined>(undefined);
+
 	return (
 		<NavigationMenu value={value} onValueChange={setValue}>
 			<NavigationMenuList>
-				<NavigationMenuItem value="shop">
-					<NavigationMenuTriggerWithFixedUX
-						onKeyboardOpen={() => setValue((value) => (value === "shop" ? undefined : "shop"))}
-					>
-						Shop
-					</NavigationMenuTriggerWithFixedUX>
-					<NavigationMenuContent>
-						<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-							<li className="row-span-3">
-								<NavigationMenuLink asChild>
-									<YnsLink
-										className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-										href="/"
-									>
-										<div className="mb-2 mt-4 text-lg font-medium">YNS</div>
-										<p className="leading-tight text-muted-foreground">
-											Your Next Store is the fastest and most compeling Stripe-powered storefront
-										</p>
-									</YnsLink>
-								</NavigationMenuLink>
-							</li>
-							<ListItem href="/products" title="All products">
-								All products in our store are listed here.
-							</ListItem>
-							<ListItem href="/category/apparel" title="Apparel">
-								All apparel products in our store.
-							</ListItem>
-							<ListItem href="/category/accessories" title="Accessories">
-								All accessories products in our store.
-							</ListItem>
-						</ul>
-					</NavigationMenuContent>
-				</NavigationMenuItem>
+				{links.map((link) => (
+					<NavigationMenuItem key={link.label} value={link.label}>
+						<NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+							<YnsLink href={link.href}>{link.label}</YnsLink>
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+				))}
 			</NavigationMenuList>
 		</NavigationMenu>
 	);
 }
-
-const ListItem = ({
-	className,
-	title,
-	children,
-	href,
-	ref,
-	...props
-}: ComponentPropsWithRef<"a">) => {
-	return (
-		<li>
-			<NavigationMenuLink asChild>
-				<YnsLink
-					ref={ref}
-					className={cn(
-						"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-						className,
-					)}
-					{...props}
-					href={href ?? "#"}
-				>
-					<div className="text-sm font-medium leading-none">{title}</div>
-					<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-				</YnsLink>
-			</NavigationMenuLink>
-		</li>
-	);
-};
-
-const NavigationMenuTriggerWithFixedUX = ({
-	onKeyboardOpen,
-	...props
-}: React.ComponentProps<typeof NavigationMenuTrigger> & {
-	onKeyboardOpen?: (e: KeyboardEvent | PointerEvent) => void;
-}) => {
-	return (
-		<NavigationMenuTrigger
-			{...props}
-			onClick={(e) => {
-				// the menu should open on click on touch screens
-				// in some browsers onClick can be triggered by PointerEvent
-				if (e.nativeEvent instanceof PointerEvent && e.nativeEvent.pointerType !== "mouse") {
-					return;
-				}
-				// prevent the default behavior for mouse users
-				e.preventDefault();
-			}}
-			// the menu should open on click on touch screens
-			onPointerDown={(e) => onKeyboardOpen?.(e)}
-			onKeyDown={(e) => {
-				// reimplement the default behavior for keyboard users
-				if (e.key === "Enter" || e.key === " ") {
-					return onKeyboardOpen?.(e);
-				}
-			}}
-		/>
-	);
-};
