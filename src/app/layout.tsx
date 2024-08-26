c7e29cd783156f7d91180fbf9b23bb35337792e4
Michal Miszczyszyn <michal@mmiszy.pl>
2024-08-26 14:31:17 +0200

    refactor: move footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages, getTranslations } from "next-intl/server";
 import { Toaster } from "@/ui/shadcn/sonner";
 import { env, publicUrl } from "@/env.mjs";
-import { Footer } from "@/ui/footer/Footer";
 
 export const generateMetadata = async (): Promise<Metadata> => {
 	const t = await getTranslations("Global.metadata");
@@ -27,8 +26,7 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
 			<body className="flex min-h-full flex-col">
 				<NextIntlClientProvider messages={messages}>
 					<div className="flex min-h-full flex-1 flex-col bg-white" vaul-drawer-wrapper="">
-						<div className="flex-1">{children}</div>
-						<Footer />
+						{children}
 					</div>
 					<Toaster position="top-center" offset={10} />
 				</NextIntlClientProvider>
