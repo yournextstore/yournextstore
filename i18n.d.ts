c3dd7fc99d3c73260d6f5cdd632e08046e537ec1
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2024-08-30 13:28:50 +0200

    chore: changes are not needed

--- i18n.d.ts
+++ i18n.d.ts
@@ -1,8 +1,6 @@
 import type en from "./messages/en.json";
-import type de from "./messages/de.json";
 
 type Messages = typeof en;
-type Messages = typeof de;
 
 declare global {
 	// Use type safe message keys with `next-intl`
