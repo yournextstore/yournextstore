f9293a66e879a5fac7fe1709653ebb8210efe29f
zaiste <oh@zaiste.net>
2024-09-19 17:16:37 +0200

    feat: remove eslint, add biome
    Co-authored-by: Michal Miszczyszyn <michal@mmiszy.pl>

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	semi: true,
-	singleQuote: false,
-	trailingComma: "all",
-	printWidth: 100,
-	useTabs: true,
-	plugins: ["prettier-plugin-tailwindcss"],
-	tailwindConfig: "./tailwind.config.ts",
-};
