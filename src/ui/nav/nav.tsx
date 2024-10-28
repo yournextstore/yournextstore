72b25072aaa5f0ca7014e0ee2637f844deb302f9
Pierre-Alexis Deville <48120835+padeville@users.noreply.github.com>
2024-10-28 22:50:12 +0100

    fix: css typo (#55)

--- src/ui/nav/nav.tsx
+++ src/ui/nav/nav.tsx
@@ -26,7 +26,7 @@ const links = [
 export const Nav = async () => {
 	return (
 		<header className="border-b py-4 z-20 sticky top-0 bg-white/90 backdrop-blur-md">
-			<div className="sm:items-centerm mx-auto flex max-w-7xl flex-col items-start gap-2 px-4 sm:flex-row sm:flex-wrap sm:items-center sm:px-6 md:flex-nowrap lg:px-8">
+			<div className="mx-auto flex max-w-7xl flex-col items-start gap-2 px-4 sm:flex-row sm:flex-wrap sm:items-center sm:px-6 md:flex-nowrap lg:px-8">
 				<YnsLink href="/">
 					<SeoH1 className="-mt-0.5 whitespace-nowrap text-xl font-bold">Your Next Store</SeoH1>
 				</YnsLink>
