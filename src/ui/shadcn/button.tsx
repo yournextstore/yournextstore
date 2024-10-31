4e2e32461b71b1ef7ab0dd2fe4b1dfafd24a2030
Grzegorz Pokorski <mr.pokorski@gmail.com>
2024-10-31 16:07:56 +0100

    fix: remove disabled attribute from add-to-cart-button component

--- src/ui/shadcn/button.tsx
+++ src/ui/shadcn/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  aria-disabled:pointer-events-none aria-disabled:opacity-50",
+	"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50",
 	{
 		variants: {
 			variant: {
