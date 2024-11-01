9b7951bd30a49bb9cb577e95a1a2f7f70c80acab
Michal Miszczyszyn <michal@mmiszy.pl>
2024-10-31 21:53:24 -0700

    feat: new mobile menu

---
+++ src/ui/nav/nav-mobile-menu.client.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/ui/shadcn/drawer";
+import { MenuIcon } from "lucide-react";
+import { type ReactNode, useState } from "react";
+
+export const NavMobileMenu = ({ children }: { children: ReactNode }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<Drawer open={isOpen} onOpenChange={setIsOpen}>
+			<DrawerTrigger>
+				<MenuIcon />
+			</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader>
+					<DrawerTitle className="text-center">Menu</DrawerTitle>
+					<DrawerDescription className="sr-only">Navigation menu</DrawerDescription>
+				</DrawerHeader>
+				<div
+					onClick={(e) => {
+						if (e.target instanceof HTMLElement && e.target.closest("a")) {
+							setIsOpen(false);
+						}
+					}}
+				>
+					{children}
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
+};
--- src/ui/shadcn/navigation-menu.tsx
+++ src/ui/shadcn/navigation-menu.tsx
@@ -35,7 +35,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-	"group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+	"group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
 );
 
 const NavigationMenuTrigger = React.forwardRef<
@@ -63,7 +63,7 @@ const NavigationMenuContent = React.forwardRef<
 	<NavigationMenuPrimitive.Content
 		ref={ref}
 		className={cn(
-			"left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
+			"left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ",
 			className,
 		)}
 		{...props}
