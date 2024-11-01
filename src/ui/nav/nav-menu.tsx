7d28fed01605351e3a41ab24ec6c3229d722cb64
Michal Miszczyszyn <michal@mmiszy.pl>
2024-10-31 21:53:24 -0700

    feat: new mobile menu

--- src/ui/nav/nav-menu.tsx
+++ src/ui/nav/nav-menu.tsx
@@ -1,38 +1,82 @@
-"use client";
-
-import * as React from "react";
-
-// TODO https://github.com/radix-ui/primitives/issues/2769
-
+import { NavMobileMenu } from "@/ui/nav/nav-mobile-menu.client";
 import {
-	NavigationMenu,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
-	navigationMenuTriggerStyle,
-} from "@/ui/shadcn/navigation-menu";
-import { YnsLink } from "@/ui/yns-link";
-import { useState } from "react";
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/ui/shadcn/drawer";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
-type NavLink = {
-	label: string;
-	href: string;
-};
+const links = [
+	{
+		label: "Home",
+		href: "/",
+	},
+	{
+		label: "Apparel",
+		href: "/category/apparel",
+	},
+	{
+		label: "Accessories",
+		href: "/category/accessories",
+	},
+	{
+		label: "Digital",
+		href: "/category/digital",
+	},
+];
 
-export function NavMenu({ links }: { links: NavLink[] }) {
-	const [value, setValue] = useState<string | undefined>(undefined);
-
-	return (
-		<NavigationMenu value={value} onValueChange={setValue} className="-ml-4 sm:ml-0">
-			<NavigationMenuList>
-				{links.map((link) => (
-					<NavigationMenuItem key={link.label} value={link.label}>
-						<NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-							<YnsLink href={link.href}>{link.label}</YnsLink>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-				))}
-			</NavigationMenuList>
-		</NavigationMenu>
-	);
+{
+	/* <Drawer>
+  <DrawerTrigger>Open</DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DrawerDescription>
+    </DrawerHeader>
+  </DrawerContent>
+</Drawer> */
 }
+
+export const NavMenu = () => {
+	return (
+		<>
+			<div className="sm:block hidden">
+				<ul className="flex flex-row items-center justify-center gap-x-1">
+					{links.map((link) => (
+						<li key={link.href}>
+							<Link
+								href={link.href}
+								className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+							>
+								{link.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="sm:hidden flex items-center">
+				<NavMobileMenu>
+					<ul className="flex pb-8 flex-col items-stretch justify-center gap-x-1">
+						{links.map((link) => (
+							<li key={link.href}>
+								<Link
+									href={link.href}
+									className="group inline-flex h-9 w-full items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+								>
+									{link.label}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</NavMobileMenu>
+			</div>
+		</>
+	);
+};
