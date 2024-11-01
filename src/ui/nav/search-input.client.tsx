8b9977a5da2472da3df78380f752980d703e5512
Michal Miszczyszyn <michal@mmiszy.pl>
2024-10-31 22:01:57 -0700

    fix: remove unused files and polish the nav

--- src/ui/nav/search-input.client.tsx
+++ src/ui/nav/search-input.client.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 
 const inputClasses = cn(
 	"appearance-none rounded-md absolute border bg-white py-2 pl-4 pr-10 w-9 opacity-0 transition-opacity ease-linear",
-	"max-smb:focus:w-[calc(100vw-2rem)] max-smb:focus:left-4 max-smb:focus:z-20 max-smb:focus:opacity-100",
+	"max-smb:focus:w-[calc(100vw-2rem)] max-smb:cursor-default max-smb:focus:left-4 max-smb:focus:z-20 max-smb:focus:opacity-100",
 	"smb:opacity-100 smb:w-full smb:pl-4 smb:pr-10 smb:inline-block smb:static",
 	"md:pl-2 md:pr-8 md:max-w-72",
 	"lg:pl-4 lg:pr-10",
