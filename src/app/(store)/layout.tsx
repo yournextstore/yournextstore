c096ae909bb638e5f2222b22bfe7eb2f063b3533
zaiste <oh@zaiste.net>
2024-11-03 20:53:45 -0800

    feat: sticky bottom on scroll product card

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -23,7 +23,7 @@ export default async function StoreLayout({
 			<Banner />
 			<Nav />
 			<TooltipProvider>
-				<main className="mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 pb-6 pt-6 sm:px-6 lg:px-8">
+				<main className="mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 pb-6 pt-2 sm:px-6 lg:px-8">
 					{children}
 					{modal}
 				</main>
