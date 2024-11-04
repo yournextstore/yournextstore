767aaf8455a7f9405b14c0c2a5c51b534a929a07
Michal Miszczyszyn <michal@mmiszy.pl>
2024-11-03 20:16:19 -0800

    fix: background -> transparent

--- src/ui/nav/nav-menu.tsx
+++ src/ui/nav/nav-menu.tsx
@@ -44,7 +44,7 @@ export const NavMenu = () => {
 						<li key={link.href}>
 							<Link
 								href={link.href}
-								className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+								className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
 							>
 								{link.label}
 							</Link>
@@ -59,7 +59,7 @@ export const NavMenu = () => {
 							<li key={link.href}>
 								<Link
 									href={link.href}
-									className="group inline-flex h-9 w-full items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+									className="group inline-flex h-9 w-full items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
 								>
 									{link.label}
 								</Link>
