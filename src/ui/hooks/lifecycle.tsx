bca800b663e6ae61c723d770e833aa86df3922aa
Michal Miszczyszyn <michal@mmiszy.pl>
2024-11-18 07:10:01 +0100

    fix: fix multiple saveBillingAddress calls on checkout

---
+++ src/ui/hooks/lifecycle.tsx
@@ -0,0 +1,17 @@
+import { type DependencyList, type EffectCallback, useEffect, useRef } from "react";
+
+export const useDidMount = (cb: EffectCallback) => useEffect(cb, []);
+
+export const useDidUpdate = (cb: EffectCallback, deps?: DependencyList) => {
+	const mountRef = useRef(false);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: on purpose
+	useEffect(() => {
+		if (!mountRef.current) {
+			mountRef.current = true;
+			return;
+		}
+
+		return cb();
+	}, deps);
+};
