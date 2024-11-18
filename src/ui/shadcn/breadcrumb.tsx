96f4014de5f1f2561489bb10a69bb8cb9006ef94
Michal Miszczyszyn <michal@mmiszy.pl>
2024-11-18 07:22:17 +0100

    style: update shadcn to use lucide

--- src/ui/shadcn/breadcrumb.tsx
+++ src/ui/shadcn/breadcrumb.tsx
@@ -1,5 +1,5 @@
-import { ChevronRightIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Slot } from "@radix-ui/react-slot";
+import { ChevronRight, MoreHorizontal } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
@@ -60,8 +60,13 @@ const BreadcrumbPage = React.forwardRef<HTMLSpanElement, React.ComponentPropsWit
 BreadcrumbPage.displayName = "BreadcrumbPage";
 
 const BreadcrumbSeparator = ({ children, className, ...props }: React.ComponentProps<"li">) => (
-	<li role="presentation" aria-hidden="true" className={cn("[&>svg]:size-3.5", className)} {...props}>
-		{children ?? <ChevronRightIcon />}
+	<li
+		role="presentation"
+		aria-hidden="true"
+		className={cn("[&>svg]:w-3.5 [&>svg]:h-3.5", className)}
+		{...props}
+	>
+		{children ?? <ChevronRight />}
 	</li>
 );
 BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
@@ -73,7 +78,7 @@ const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span"
 		className={cn("flex h-9 w-9 items-center justify-center", className)}
 		{...props}
 	>
-		<DotsHorizontalIcon className="h-4 w-4" />
+		<MoreHorizontal className="h-4 w-4" />
 		<span className="sr-only">More</span>
 	</span>
 );
