0a6c595fb14aea558dcbc14d1426b767289696a8
Jakub Neander <oh@zaiste.net>
2024-12-13 12:22:07 +0100

    refactor: drop parallel routes for cart drawer
    Co-Authored-By: Michal Miszczyszyn <michal@mmiszy.pl>

--- src/app/(store)/cart/cart-aside-drawer.tsx
+++ src/app/(store)/cart/cart-aside-drawer.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useCartModal } from "@/context/cart-modal";
 import { Drawer, DrawerContent, DrawerTitle } from "@/ui/shadcn/drawer";
 import { useMediaQuery } from "@/ui/shadcn/hooks/use-media-query";
 import { useRouter } from "next/navigation";
-import { type ReactNode, useEffect, useRef, useState } from "react";
+import { type ReactNode, useEffect } from "react";
 
 export const CartAsideDrawer = ({ children }: { children: ReactNode }) => {
-	const [open, setOpen] = useState(true);
 	const router = useRouter();
+	const { open, setOpen } = useCartModal();
 
 	useEffect(() => {
 		const onKeyDown = (event: KeyboardEvent) => {
@@ -19,26 +20,10 @@ export const CartAsideDrawer = ({ children }: { children: ReactNode }) => {
 		return () => document.removeEventListener("keydown", onKeyDown);
 	}, [router]);
 
-	const timerId = useRef<ReturnType<typeof setTimeout> | null>(null);
-
 	const isDesktop = useMediaQuery("(min-width: 640px)");
 
 	return (
-		<Drawer
-			open={open}
-			shouldScaleBackground={true}
-			direction={isDesktop ? "right" : "bottom"}
-			onOpenChange={(e) => {
-				if (!e) {
-					if (timerId.current) {
-						clearTimeout(timerId.current);
-					}
-					timerId.current = setTimeout(() => {
-						router.back();
-					}, 100);
-				}
-			}}
-		>
+		<Drawer open={open} shouldScaleBackground={true} direction={isDesktop ? "right" : "bottom"}>
 			<DrawerTitle className="sr-only">Shopping cart</DrawerTitle>
 			<DrawerContent
 				className="sm:fixed sm:bottom-0 sm:left-auto sm:right-0 sm:top-0 sm:mt-0 sm:flex sm:h-full sm:w-1/2 sm:flex-col sm:overflow-hidden sm:rounded-none sm:bg-white sm:shadow-xl lg:w-1/3"
