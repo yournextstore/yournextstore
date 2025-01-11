0b9a275dcd90af444604072e0c0e66962ca84818
Jakub Neander <oh@zaiste.net>
2025-01-11 15:49:17 +0100

    feat: simple jwt-in-cookie auth

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { decrypt, updateSession } from "./lib/auth";
+
+const ProtectedPaths = ["/orders"];
+
+export async function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+	const isProtectedPath = ProtectedPaths.some((p) => pathname.startsWith(p));
+
+	if (!isProtectedPath) {
+		return NextResponse.next();
+	}
+
+	const session = request.cookies.get("session")?.value;
+	if (!session) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	const data = await decrypt(session);
+	if (!data || data.expires < Date.now()) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	return updateSession(request);
+}
+
+export const config = {
+	matcher: ["/orders"],
+};
