c21f5d0f33ddaf8c7fb24e689043b3f091e2192d
Jakub Neander <oh@zaiste.net>
2025-01-23 22:43:34 +0100

    feat: migrate to Tailwind 4

--- src/ui/login-form.tsx
+++ src/ui/login-form.tsx
@@ -37,7 +37,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
 					</form>
 				</CardContent>
 			</Card>
-			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
 				By clicking continue, you agree to our <a href="#">Terms of Service</a> and{" "}
 				<a href="#">Privacy Policy</a>.
 			</div>
--- src/ui/nav/nav-menu.tsx
+++ src/ui/nav/nav-menu.tsx
@@ -26,7 +26,7 @@ export const NavMenu = () => {
 						<li key={link.href}>
 							<Link
 								href={link.href}
-								className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+								className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-hidden"
 							>
 								{link.label}
 							</Link>
@@ -41,7 +41,7 @@ export const NavMenu = () => {
 							<li key={link.href}>
 								<Link
 									href={link.href}
-									className="group inline-flex h-9 w-full items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none"
+									className="group inline-flex h-9 w-full items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-hidden"
 								>
 									{link.label}
 								</Link>
