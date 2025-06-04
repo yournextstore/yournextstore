68907b4dde7512175187a3efdfe4489448eba444
Michal Miszczyszyn <michal@mmiszy.pl>
2025-06-04 11:02:33 +0200

    fix: robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,20 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: "*",
-			allow: "/",
-		},
+		rules: [
+			{
+				userAgent: "*",
+				allow: "/",
+			},
+			{
+				userAgent: "Googlebot",
+				disallow: " ",
+			},
+			{
+				userAgent: "Googlebot-image",
+				disallow: " ",
+			},
+		],
 		sitemap: publicUrl + "/sitemap.xml",
 	};
 }
