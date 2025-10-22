f7a38d7157670dd1a1eddae0ff57778cc826b2ca
Michal Miszczyszyn <michal@mmiszy.pl>
2025-10-22 12:02:38 -0700

    feat: new-sdk wip

--- src/lib/api.ts
+++
@@ -1,2 +0,0 @@
-import * as Search from "./search/search";
-export { Search };
--- src/ui/markdown.tsx
+++
@@ -1,7 +0,0 @@
-import "server-only";
-
-import { MDXRemote } from "next-mdx-remote/rsc";
-
-export const Markdown = async ({ source }: { source: string }) => {
-	return <MDXRemote source={source} options={{ mdxOptions: { format: "md" } }} />;
-};
