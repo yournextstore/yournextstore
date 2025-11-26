ff3e9234542621a59a6b1866a86fdcb52cd96037
Jakub Neander <oh@zaiste.net>
2025-11-26 17:15:30 +0100

    chore: lint

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -189,7 +189,7 @@ function FieldError({
 
 		const uniqueErrors = [...new Map(errors.map((error) => [error?.message, error])).values()];
 
-		if (uniqueErrors?.length == 1) {
+		if (uniqueErrors?.length === 1) {
 			return uniqueErrors[0]?.message;
 		}
 
