71c6d85056291a932fe99b7f3f88dd7b81a77645
Jakub Neander <oh@zaiste.net>
2025-11-30 17:04:45 +0100

    refactor: coherent config

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function isVideoUrl(url: string): boolean {
