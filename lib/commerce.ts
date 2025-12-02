6bd1d30f706ddc520ff13e48131a621908794b2f
Michal Miszczyszyn <michal@mmiszy.pl>
2025-12-02 20:36:06 +0100

    YNS_API_KEY

--- lib/commerce.ts
+++ lib/commerce.ts
@@ -2,5 +2,5 @@ import { Commerce } from "commerce-kit";
 
 export const commerce = Commerce({
 	endpoint: process.env.NEXT_PUBLIC_YNS_API_TENANT,
-	token: process.env.YNS_API_TOKEN,
+	token: process.env.YNS_API_KEY,
 });
