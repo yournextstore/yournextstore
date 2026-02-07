23faa997cf6c2662d0cb61ca209e71b9e57e3bcf
Jakub Neander <oh@zaiste.net>
2026-02-06 16:56:34 -0800

    feat: navigate reporter for back/forward

---
+++ components/navigation-reporter.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Script from "next/script";
+import { useEffect } from "react";
+
+export function NavigationReporter() {
+	const router = useRouter();
+
+	useEffect(() => {
+		if (typeof window === "undefined" || window.parent === window) return;
+
+		const handleMessage = (event: MessageEvent) => {
+			if (event.data?.type === "navigate" && typeof event.data.pathname === "string") {
+				router.push(event.data.pathname);
+			}
+		};
+
+		window.addEventListener("message", handleMessage);
+		return () => window.removeEventListener("message", handleMessage);
+	}, []);
+
+	return (
+		<Script id="navigation-reporter" strategy="afterInteractive">
+			{`
+(function() {
+  if (window.parent === window) return;
+
+  function reportNavigation() {
+    window.parent.postMessage({ type: "navigation", pathname: window.location.pathname }, "*");
+  }
+
+  reportNavigation();
+
+  var pushState = history.pushState;
+  var replaceState = history.replaceState;
+
+  history.pushState = function() {
+    pushState.apply(this, arguments);
+    reportNavigation();
+  };
+
+  history.replaceState = function() {
+    replaceState.apply(this, arguments);
+    reportNavigation();
+  };
+
+  window.addEventListener("popstate", reportNavigation);
+})();
+      `}
+		</Script>
+	);
+}
