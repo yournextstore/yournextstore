738b13c031c1d8940620e3ca1a043ef887310f0d
Jakub Neander <oh@zaiste.net>
2026-08-10 17:38:31 +0200

    chore(robots): drop disallow for the nonexistent /cart route
    The cart is a sidebar; there is no /cart page to exclude.

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
 			{
 				userAgent: "*",
 				allow: "/",
-				disallow: ["/cart", "/checkout", "/order/", "/search", "/api/"],
+				disallow: ["/checkout", "/order/", "/search", "/api/"],
 			},
 		],
 		sitemap: `${baseUrl}/sitemap.xml`,
